319dc16d266a5b39a427ba89ee173e51fa321b50
DAP <dlrayjr89@gmail.com>
2020-04-23 20:37:11 +0000

    depend installed

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//
+import { ApolloProvider} from "react-apollo"
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

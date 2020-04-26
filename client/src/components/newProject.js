09383feb1d0aec16f5cfff1cc4bb5984fc2e5963
Dlray89 <dlrayjr89@gmail.com>
2020-04-26 01:01:53 -0400

    update

--- client/src/components/newProject.js
+++ client/src/components/newProject.js
@@ -7,8 +7,8 @@ import { Mutation } from "react-apollo"
 import { FEED_QUERY } from "./ProjectList"
 import gql from "graphql-tag"
 
-const POST_MUTATION = gql `
-mutation PostMutation($projectName: String!, $description: String!) {
+const POST_MUTATION = gql`
+mutation addproject($projectName: projectName, $description: String!) {
     post(projectName: $projectName, description: $description) {
         id
         projectName
@@ -23,40 +23,70 @@ class NewProject extends Component {
         description: ''
     }
 
+    updateProjectName = ({ target: { value } }) => {
+        this.setState({ projectName: value })
+    }
+
+    updateDescription = ({ target: { value } }) => {
+        this.setState({ description: value })
+    }
+
+    resetForm = () => {
+        this.setState({ projectName: "", description: "" })
+    }
+
     render() {
-        const { projectName, description} = this.state
-        return(
+        return (
             <div>
-                <div>
-                        <input 
-                            value={projectName}
-                            onChange={e => this.setState({ projectName: e.target.value})}
-                            type="text"
-                            placeholder="Name"
-                        />
-                        <input 
-                            value={description}
-                            onChange={e => this.setState({ description: e.target.value})}
-                            type="text"
-                            placeholder="Project details"
-                        />
-                        <Mutation
-                         mutation={POST_MUTATION} 
-                         variables={{ projectName, description}}
-                        //  onCompleted={() => this.props.push()
-                         update={(store, {data: { post } }) => {
-                             const data = store.readQuery({ query: FEED_QUERY })
-                             data.feed.projects.unShift(post)
-                             store.writeQuery({
-                                 query: FEED_QUERY,
-                                 data
-                             })
-                         }}
-                         >
-                        {postMutation => <button onClick={postMutation}>Submit</button>}
-                        </Mutation>
-                       
-                </div>
+                <Mutation
+                    mutation={POST_MUTATION}
+                    refetchQueries={[
+                        {
+                            query: FEED_QUERY,
+                            variables: { projectName: String, description: String }
+                        }
+                    ]}
+                >
+                    {(addProject, { loading, error }) => (
+
+
+                        <form
+                            onSubmit={e => {
+                                e.preventDefault();
+                                addProject({
+                                    variables: {
+                                        project: {
+                                            projectName: this.state.projectName,
+                                            description: this.state.description
+                                        }
+                                    }
+                                })
+
+
+                                this.resetForm()
+                            }}>
+                            <input
+                                value={this.state.projectName}
+                                onChange={this.updateProjectName}
+                                type="text"
+                                placeholder="Name"
+                            />
+                            <input
+                                value={this.state.description}
+                                onChange={this.updateDescription}
+                                type="text"
+                                placeholder="Project details"
+                            />
+                            <div>
+                                <button>Add project</button>
+                                {loading && <p>Loading...</p>}
+                                {error && <p>Error...</p>}
+                            </div>
+
+
+                        </form>
+                    )}
+                </Mutation>
             </div>
         )
     }

b2a9895f26ac2b1180d777b0c62cd526cc706ed4
DAP <dlrayjr89@gmail.com>
2020-05-03 06:03:55 +0000

    git package added along with package.json

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -5,6 +5,15 @@ function post(parent, args, context, info) {
     })
 }
 
+function remove(parent, args, context, info) {
+    return context.prisma.deleteProject({
+        where: {id: args.id }
+    },
+    info
+    )
+}
+
 module.exports = {
     post,
+    remove,
 }
